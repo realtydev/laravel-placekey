d44f378b1228309a4f7ffe917e823b5177b0a1b4
Alex Gibson <35852411+realtydev@users.noreply.github.com>
2023-11-09 23:28:35 -0500

    Update LaravelPlacekey.php

--- src/LaravelPlacekey.php
+++ src/LaravelPlacekey.php
@@ -2,6 +2,16 @@
 
 namespace Realtydev\LaravelPlacekey;
 
+use Realtydev\LaravelPlacekey\Services\PlacekeyService;
+
 class LaravelPlacekey
 {
+    protected $service;
+
+    public function __construct(PlacekeyService $service)
+    {
+        $this->service = $service;
+    }
+
+    // Add methods to interact with the service
 }
