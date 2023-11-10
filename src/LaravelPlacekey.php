4eb095a98d05c74ca082d889ed593fb589977459
Alex Gibson <35852411+realtydev@users.noreply.github.com>
2023-11-09 23:45:58 -0500

    Update LaravelPlacekey.php

--- src/LaravelPlacekey.php
+++ src/LaravelPlacekey.php
@@ -13,8 +13,16 @@ class LaravelPlacekey
         $this->service = app(PlacekeyService::class);
     }
 
+    public function getPlacekeyForCoordinates($latitude, $longitude, $queryId = null)
+    {
+        return $this->service->getPlacekeyForCoordinates($latitude, $longitude, $queryId);
+    }
     public function getPlacekeyForAddress($street, $city, $region, $postal_code, $countryCode)
     {
         return $this->service->getPlacekeyForAddress($street, $city, $region, $postal_code, $countryCode);
+    }
+    public function getCurrentActivePlacekeyAndPredecessors($placekeys)
+    {
+        return $this->service->getCurrentActivePlacekeyAndPredecessors($placekeys);
     }
 }
